213318e41e16b34ba2500c4d8252616303983ea5
nicolewhite <nicole.white@neotechnology.com>
2015-06-14 09:52:07 -0700

    initial

---
+++ generate.R
@@ -0,0 +1,8 @@
+docs = list.files("man")
+
+for (i in 1:length(docs)) {
+  input_file = paste("man", docs[i], sep="/")
+  output_file = paste0(sub(".Rd", ".html", docs[i]))
+  command = paste("R CMD Rdconv -o", output_file, "--type=html", input_file)
+  system(command)
+}
